a8f085c28b3eb2c775127681d554d64c0deb5e66
Inna Shishova <sh201608@yandex.ru>
2025-11-04 21:11:37 +0400

    added RegFormTestsPageObgects

--- qaguru-in-sh-lesson5/src/test/java/RegFormTestsPageObgects.java
+++ qaguru-in-sh-lesson5/src/test/java/RegFormTestsPageObgects.java
@@ -2,28 +2,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
-public class RegFormTestsPageObgects {
+public class RegFormTestsPageObgectsTests {
 
-    @BeforeAll
-    static void prepareEnvironment() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 3000;
+      @Test
 
-    }
+    void successfulRegistrationTest() {
 
-    @Test
-    //если все заполнено корректно//
-    void fillFormTest() {
-        //Открытие страницы 'Practice Form'//
-     //   open("/automation-practice-form");
         registrationPage.open()
                 .setFirstName("Ivan")
                 .setLastName("Petrov")
@@ -39,9 +24,6 @@ public class RegFormTestsPageObgects {
                 .setCity("Panipat")
                 .submit();
 
-
-
-        //Проверка таблицы
         registrationPage.checkResultTitle("Thanks for submitting the form")
                 .checkResultCells("Student Name", "Ivan Petrov")
                 .checkResultCells("Student Email", "test1@test2.com")
